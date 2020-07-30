1c333032e0d49685f78175ceb64ae6ae24d8972f
chanhduypq <chanhduypq@gmail.com>
2020-07-30 23:49:09 +0700

    tạo Route cho việc quản lý một cái gì đó, ví dụ này là quản lý class

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/class', [
+    'uses' => 'ClassController@index',
+    'as' => 'class'
+]);
+Route::get('/class/create', [
+    'uses' => 'ClassController@create',
+    'as' => 'class.create'
+]);//;
+Route::post('/class/store', [
+    'uses' => 'ClassController@store',
+    'as' => 'class.store'
+]);
+Route::get('/class/edit/{primaryKeyValue}', [
+    'uses' => 'ClassController@edit',
+    'as' => 'class.edit'
+]);
+Route::put('/class/update/{primaryKeyValue}', [
+    'uses' => 'ClassController@update',
+    'as' => 'class.update'
+]);
+Route::delete('/class/delete/{primaryKeyValue}', [
+    'uses' => 'ClassController@delete',
+    'as' => 'class.delete'
+]);
