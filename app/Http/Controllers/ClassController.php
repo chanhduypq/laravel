5b18c609d0ab12bbeabffcb82e92681bbd01cb5d
chanhduypq <chanhduypq@gmail.com>
2020-07-31 00:25:49 +0700

    code cho việc hiển thị danh sách một cái gì đó, ví dụ này là danh sách class

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -15,6 +15,8 @@ class ClassController extends Controller
     public function index()
     {
         //
+        $items = Classes::paginate(5);
+        return view('classes.index', array('items' => $items));
     }
 
     /**
---
+++ resources/views/classes/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.pupil')
+
+@section('title', 'Class')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>
+                                id
+                            </th>
+                            <th>
+                                Name
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($items as $class)
+                        <tr>
+                            <td>
+                                {{ $class->id }}
+                            </td>
+                            <td>
+                                {{ $class->name }}
+                            </td>
+                        </tr>
+                        @endforeach 
+                    </tbody>
+
+                </table>
+            </div>
+
+            </form>
+            <div class="row">
+                <div class="col-sm-5">&nbsp;</div>
+                <div class="col-sm-6">
+                    {{ $items->links() }}
+                </div>
+                <div class="col-sm-1">&nbsp;</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
