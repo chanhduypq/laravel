8f3b21055e6ff09b3203232b749b742590a6c05b
chanhduypq <chanhduypq@gmail.com>
2020-07-31 07:15:48 +0700

    css, js, images, bootstrap, jquery,...

--- resources/views/layouts/pupil.blade.php
+++ resources/views/layouts/pupil.blade.php
@@ -2,6 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <title>@yield('title')</title>
+        <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" />
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap/bootstrap.css') }}" />
     </head>
     <body>
         <div class="container">
@@ -9,5 +11,12 @@
                 @yield('content')
             </div>
         </div>
+        
+        <script type="text/javascript" src="{{ URL::asset('js/jquery-2.0.3.js') }}"></script>
+        <script src="{{ URL::asset('js/bootstrap/bootstrap.bundle.js') }}"></script>
+        <script src="{{ URL::asset('js/bootstrap/bootstrap.min.js') }}"></script>
+        
+        @yield('script')
+        @yield('script_page')
     </body>
 </html>
