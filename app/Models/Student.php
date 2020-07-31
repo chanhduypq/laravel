52c45c8836f07ca21c5281892cfac3d33acf1b89
chanhduypq <chanhduypq@gmail.com>
2020-07-31 22:18:47 +0700

    sau khi thêm/xóa/sửa một table, rồi làm tiếp một việc nào đó với table khác

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    public static function boot() {
+        parent::boot();
+
+        self::creating(function($model) {
+            
+        });
+
+        self::created(function($model) {
+            $classModel = Classes::find($model->class_id);
+            $classModel->number_of_student++;
+            $classModel->save();
+        });
+
+        self::updating(function($model) {
+        });
+
+        self::updated(function($model) {
+        });
+
+        self::deleting(function($model) {
+        });
+
+        self::deleted(function($model) {
+        });
+    }
 }
