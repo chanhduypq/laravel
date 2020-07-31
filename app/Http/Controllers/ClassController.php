edf08bb7ea9c785ce97c5bb87c521a19cf823b55
chanhduypq <chanhduypq@gmail.com>
2020-07-31 09:36:54 +0700

    code create một table nào đó, ví dụ này là table classes

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Classes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ClassController extends Controller
 {
@@ -27,6 +28,7 @@ class ClassController extends Controller
     public function create()
     {
         //
+        return view('classes.create');
     }
 
     /**
@@ -38,6 +40,41 @@ class ClassController extends Controller
     public function store(Request $request)
     {
         //
+        $model = new Classes();
+        $rulesForAddingValidattion = [
+            'name' => [
+                'required',
+                'max:30',
+                'min:2',
+                'unique:classes',
+            ],
+        ];
+        $validattionErrorMessages = [
+            'name.required' => 'Vui lòng nhập tên lớp.',
+            'name.max' => 'Tên lớp không được dài quá 30 kí tự',
+            'name.min' => 'Tên lớp không được ít hơn 2 kí tự',
+            'name.unique' => 'Đã tồn tại lớp học mang tên này rồi',
+        ];
+        $validator = Validator::make($request->all(), $rulesForAddingValidattion, $validattionErrorMessages);
+
+        if ($validator->fails()) {
+            return redirect('class/create')
+                            ->withErrors($validator->errors())
+                            ->withInput();
+        } else {
+            /**
+             * save on database
+             */
+            $columnsInTable = \Illuminate\Support\Facades\Schema::getColumnListing('classes');
+            foreach ($request->all() as $key => $value) {
+                if(in_array($key, $columnsInTable)){
+                    $model->$key = $value;
+                }
+            }
+            $model->save();
+            //
+            return redirect('class');
+        }
     }
 
     /**
