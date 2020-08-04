fe9f79a2ab369a30bd8e79531fef73dbdc0ee859
chanhduypq <chanhduypq@gmail.com>
2020-08-04 19:34:58 +0700

    no message

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -9,10 +9,6 @@ use Illuminate\Validation\Rule;
 
 class ClassController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
     /**
      * Display a listing of the resource.
      *
