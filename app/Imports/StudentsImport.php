8a15af61b56602e77a87dd72e366ddf4fa222acb
chanhduypq <chanhduypq@gmail.com>
2020-08-11 23:24:37 +0700

    excel chunk

--- app/Imports/StudentsImport.php
+++ app/Imports/StudentsImport.php
@@ -3,9 +3,10 @@
 namespace App\Imports;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 use App\Models\Student;
 
-class StudentsImport implements ToModel, WithHeadingRow
+class StudentsImport implements ToModel, WithHeadingRow, WithChunkReading
 {
     /**
     * @param array $row
@@ -20,6 +21,10 @@ class StudentsImport implements ToModel, WithHeadingRow
         ]);
     }
     
+    public function chunkSize(): int {
+        return 1000;
+    }
+
     public function headingRow(): int {
         return 1;//return 1, số 1 có nghĩa là header ở dòng 1, còn nếu header ở dòng 2 thì sẽ là return 2
     }
